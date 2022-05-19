d11d5f7b9d0cc4303ccf275ec5a2a8f57b7c5e1e
Kenneth <obikennethe913@gmail.com>
2022-05-19 12:55:10 +0300

    redux basic completed

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 import Counter from "./components/Counter"
 import Header from "./components/Header"
 import Auth from "./components/Auth"
-
+import { useSelector } from "react-redux"
+import UserProfile from "./components/UserProfile"
 function App() {
+  const auth = useSelector((value) => value.auth.isAuthenticated)
   return (
     <>
       <Header />
-      <Auth />
+      {!auth && <Auth />}
+      {auth && <UserProfile />}
       <Counter />
     </>
   )
--- src/components/Auth.js
+++ src/components/Auth.js
@@ -1,38 +1,45 @@
-import { useDispatch, useSelector } from "react-redux"
+import { useDispatch } from "react-redux"
 import classes from "./Auth.module.css"
 import { authActions } from "../store"
-import UserProfile from "./UserProfile.js"
+import { useRef } from "react"
 
 const Auth = () => {
+  const emailRefInput = useRef()
+  const passwordRefInput = useRef()
   const dispatch = useDispatch()
-  const auth = useSelector((value) => value.auth.isAuthenticated)
 
   const submitHandler = (e) => {
     e.preventDefault()
+    const enteredEmail = emailRefInput.current.value
+    const enteredPassword = passwordRefInput.current.value
+
+    const enteredEmailIsValid = enteredEmail.trim().includes("@")
+    const enteredPasswordIsValid = enteredPassword.trim().length > 5
+
+    let formIsValid = false
+    if (enteredEmailIsValid && enteredPasswordIsValid) formIsValid = true
+
+    if (!formIsValid) return
+
     dispatch(authActions.login())
   }
 
   return (
-    <>
-      {auth && <UserProfile />}
-      {!auth && (
-        <main className={classes.auth}>
-          <section>
-            <form onSubmit={submitHandler}>
-              <div className={classes.control}>
-                <label htmlFor="email">Email</label>
-                <input type="email" id="email" />
-              </div>
-              <div className={classes.control}>
-                <label htmlFor="password">Password</label>
-                <input type="password" id="password" />
-              </div>
-              <button>Login</button>
-            </form>
-          </section>
-        </main>
-      )}
-    </>
+    <main className={classes.auth}>
+      <section>
+        <form onSubmit={submitHandler}>
+          <div className={classes.control}>
+            <label htmlFor="email">Email</label>
+            <input type="email" id="email" ref={emailRefInput} />
+          </div>
+          <div className={classes.control}>
+            <label htmlFor="password">Password</label>
+            <input type="password" id="password" ref={passwordRefInput} />
+          </div>
+          <button>Login</button>
+        </form>
+      </section>
+    </main>
   )
 }
 
